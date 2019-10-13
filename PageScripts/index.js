bf3af9d04c3e801d762839f19b2b02a15c7223a2
Devillyna <karina.temchur@gmail.com>
2019-10-13 13:37:26 +0300

    first commit

---
+++ PageScripts/index.js
@@ -0,0 +1,14 @@
+(_ => {
+  const articles = [
+    {
+      title: "Sport life of Klichko",
+      content: {
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur omnis accusantium sit earum, odio reiciendis quas, provident vitae voluptates, obcaecati nulla enim nisi veniam rem perferendis inventore quam, cumque dolores. Amet minus est, sit, optio deserunt",
+        media: {
+          image: "./Pictures/Cat.png"
+        }
+      }
+    }
+  ];
+})();
