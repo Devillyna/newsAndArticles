53cfbdb2700b92cbe2742bcc7c8cadb891d7afb5
Devillyna <karina.temchur@gmail.com>
2019-10-28 20:31:20 +0200

    delete rudement code and add piece of comments

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,8 @@ import '../css/style.css';
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
-import '@fortawesome/fontawesome-free/js/brands';
-
-import '@babel/polyfill';
+import '@fortawesome/fontawesome-free/js/brands'; // icons
+import '@babel/polyfill'; // library for crossbrowsing
 
 import { Article } from './components/article';
 import { Modal } from './components/modal';
@@ -14,70 +13,79 @@ import { articles } from './data';
 import { readFile } from './utilities';
 
 const articleManager = new ArticlesManager(
-  articles,
-  'article-component',
-  document.getElementById('articles_container')
+  articles, // массив статей
+  'article-component', // имя компонента для одной статьи
+  document.getElementById('articles_container') // где отрисовываются статьи
 );
 
-articleManager.setIdGenerator(_ => Date.now());
+articleManager.setIdGenerator(() => Date.now()); // передаю функцию, которая будет генерировать идентификаторы
 
 function articleGenerator({ title = null, content = null } = {}) {
+  // В эту функцию передается объект со свойствами title и content. С помощью конструкции {title, content}
+  // я достаю из него эти значения и присваиваю переменным title и content (теперь это обычные переменные)
+  // через оператор = я указываю параметры по умолчанию:
+  // - если в функцию вместо объекта будет передано свойство undefined, он станет пустым объектом
+  // - если в объекте одно из указанных свойств будет равно undefined, они станут равны null
+
   return new Promise(async (resolve, reject) => {
-    let modalElement = document.getElementsByTagName('modal-component')[0];
+    // в конструктор объекта "Обещание" передаются его методы resolve и reject
+    // он нужен для асинхронных событий т.е. тех, которые выполнятся не сразу
+    // при вызове функции resolve Promise переходит в состояние "исполненно"
+    // и из него возвращается значение переданное в качестве аргумента
+    // привызове функции reject он переходит в состояние "отклоненно"
+    // и вызывается исключение
 
-    if (!modalElement) {
-      modalElement = document.createElement('modal-component');
+    const modalElement = document.createElement('modal-component');
 
-      const modalInner = document.createElement('div');
-      modalInner.classList.add('modal-inner');
+    const modalInner = document.createElement('div');
+    modalInner.classList.add('modal-inner');
 
-      const titleInput = document.createElement('div');
-      titleInput.setAttribute('placeholder', 'title');
-      titleInput.contentEditable = true;
-      titleInput.innerHTML = title;
+    const titleInput = document.createElement('div');
+    titleInput.setAttribute('placeholder', 'title');
+    titleInput.contentEditable = true;
+    titleInput.innerHTML = title;
 
-      const textInput = document.createElement('div');
-      textInput.contentEditable = true;
-      textInput.setAttribute('placeholder', 'text');
+    const textInput = document.createElement('div');
+    textInput.contentEditable = true;
+    textInput.setAttribute('placeholder', 'text');
 
-      if (content) {
-        textInput.innerHTML = content.text;
-      }
-
-      const fileInput = document.createElement('input');
-      fileInput.type = 'file';
-      fileInput.accept = 'image/png, image/jpeg, audio/mp3, video/mp4';
-
-      const acceptButton = document.createElement('button');
-      acceptButton.innerHTML = 'send';
-
-      acceptButton.addEventListener('click', async _ => {
-        const title = titleInput.innerText.trim();
-        const text = textInput.innerHTML;
-
-        const file = fileInput.files[0];
-        const media = file ? await readFile(file) : content && content.media;
-
-        const type = file
-          ? file.type.split('/').shift()
-          : content && content.type;
-
-        if (title !== '' && textInput.innerText.trim() !== '') {
-          modalElement.remove();
-
-          resolve({ title, content: { text, media, type } });
-        }
-      });
-
-      modalInner.append(titleInput);
-      modalInner.append(textInput);
-      modalInner.append(fileInput);
-      modalInner.append(acceptButton);
-
-      modalElement.append(modalInner);
-
-      document.body.append(modalElement);
+    if (content) {
+      textInput.innerHTML = content.text;
     }
+
+    const fileInput = document.createElement('input');
+    fileInput.type = 'file';
+    fileInput.accept = 'image/png, image/jpeg, audio/mp3, video/mp4';
+
+    const acceptButton = document.createElement('button');
+    acceptButton.innerHTML = 'send';
+
+    acceptButton.addEventListener('click', async _ => {
+      const title = titleInput.innerText.trim();
+      const text = textInput.innerHTML;
+
+      const file = fileInput.files[0];
+      const media = file ? await readFile(file) : content && content.media;
+
+      const type = file
+        ? file.type.split('/').shift()
+        : content && content.type;
+
+      if (title !== '' && textInput.innerText.trim() !== '') {
+        modalElement.remove();
+
+        resolve({ title, content: { text, media, type } });
+      }
+    });
+
+    modalInner.append(titleInput);
+    modalInner.append(textInput);
+    modalInner.append(fileInput);
+    modalInner.append(acceptButton);
+
+    modalElement.append(modalInner);
+
+    document.body.append(modalElement);
   });
 }
 
